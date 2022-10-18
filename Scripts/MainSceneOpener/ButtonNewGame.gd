8b874ffab0a6c4a6eb7d93a6fd61488fa1a7d809
Ernest Gupik <ernestgupik@wp.pl>
2022-10-18 21:47:58 +0200

    Napraw przycisk nowej gry
    Po zmianie nazwy "Scene1.tscn" na "TestLevel.tscn"

--- Scripts/MainSceneOpener/ButtonNewGame.gd
+++ Scripts/MainSceneOpener/ButtonNewGame.gd
@@ -2,4 +2,4 @@ extends Button
 
 
 func _on_ButtonNewGame_pressed():
-	assert(get_tree().change_scene("res://Scenes/Scene1.tscn") == OK)
+	assert(get_tree().change_scene("res://Scenes/Levels/TestLevel.tscn") == OK)
