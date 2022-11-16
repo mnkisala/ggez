ae275c8f22a640c2681a98d2e886d31ed9633019
mnkisala <mnkisala@gmail.com>
2022-11-17 00:45:43 +0100

    mruganie portali

--- Scripts/Portal.gd
+++ Scripts/Portal.gd
@@ -3,17 +3,31 @@ extends Spatial
 export(LevelEnum.Level) var target = LevelEnum.Level.NULL
 var __active = false
 
+const COLOR_ON = Color(0.3, 1.0, 0.1, 0.3)
+const COLOR_OFF = Color(1.0, 0.1, 0.2, 0.3)
+
+const BASE_LIGHT = 5.0
+
+
 func _ready():
 	get_node("Particles").process_material = get_node("Particles").process_material.duplicate()
 
 	if GameManager.isPortalTargetActive(target):
-		get_node("Particles").process_material.color = Color(0, 1.0, 0, 0.15)
-		get_node("OmniLight").light_color = Color(0, 1.0, 0, 0.15)
+		get_node("Particles").process_material.color = COLOR_ON
+		get_node("OmniLight").light_color = COLOR_ON
 	else:
-		get_node("Particles").process_material.color = Color(1.0, 0.0, 0, 0.15)
-		get_node("OmniLight").light_color = Color(1.0, 0, 0, 0.15)
+		get_node("Particles").process_material.color = COLOR_OFF
+		get_node("OmniLight").light_color = COLOR_OFF
 
 
-func _on_Area_body_entered(body:Node):
+func _process(dt):
+	# mrygansko
+	var light = get_node("OmniLight")
+	light.light_energy = lerp(
+		light.light_energy, BASE_LIGHT + (randf() * 2 - 1.0) * 3.0, dt * 20.0 * randf()
+	)
+
+
+func _on_Area_body_entered(body: Node):
 	print("teleportation: ", body, " -> ", target)
 	GameManager.changeScene(target)
