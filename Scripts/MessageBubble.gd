ffe1267cd32a1b86a44892be478c63e95556571b
Ernest Gupik <ernestgupik@wp.pl>
2022-11-18 20:04:46 +0100

    MessageBubble: troche popsuta implementacja zmiany font size'a
    z poziomu editora (w instancjach messagebubble)

--- Scripts/MessageBubble.gd
+++ Scripts/MessageBubble.gd
@@ -1,6 +1,10 @@
 extends Spatial
 
 export(String, MULTILINE) var text setget set_text
+export(int) var font_size setget set_font_size
 	
 func set_text(text):
 	get_node("Label3D").text = text
+
+func set_font_size(font_size):
+	get_node("Label3D").pixel_size = float(font_size) / 100
