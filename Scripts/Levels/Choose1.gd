70548caafdda4b376a397367c1df8b736ff81fe0
mnkisala <mnkisala@gmail.com>
2022-11-20 01:43:18 +0100

    wyswietlanie punktow w choose1

--- Scripts/Levels/Choose1.gd
+++ Scripts/Levels/Choose1.gd
@@ -1,41 +1,17 @@
 extends Spatial
 
 var questions = [
-	[
-		"W jaki sposób można dbać o środowisko?",
-		"jeżdżąc na rowerze",
-		"jeżdżąc samochodem"
-	],
-	[
-		"Do jakiego kosza na śmieci należy wrzucać plastik?",
-		"żółtego",
-		"zielonego"
-	],
-	[
-		"Co to jest recykling?",
-		"powtórne wykorzystanie odpadów",
-		"zbieranie śmieci w lesie"
-	],
-	[
-		"Do jakiego kosza na śmieci należy wrzucać szkło?",
-		"zielonego",
-		"żółtego"
-	],
+	["W jaki sposób można dbać o środowisko?", "jeżdżąc na rowerze", "jeżdżąc samochodem"],
+	["Do jakiego kosza na śmieci należy wrzucać plastik?", "żółtego", "zielonego"],
+	["Co to jest recykling?", "powtórne wykorzystanie odpadów", "zbieranie śmieci w lesie"],
+	["Do jakiego kosza na śmieci należy wrzucać szkło?", "zielonego", "żółtego"],
 	[
 		"Dlaczego kartonowe pudełka należy\nzgniatać przed wyrzuceniem?",
 		"żeby zajmowały mniej miejsca na wysypisku",
 		"wtedy szybciej sie rozłożą"
 	],
-	[
-		"Do jakiego kosza na śmieci należy wrzucać papier?",
-		"niebieskiego",
-		"żółtego"
-	],
-	[
-		"Czym są zielone płuca ziemii?",
-		"to lasy",
-		"to największe rzeki"
-	],
+	["Do jakiego kosza na śmieci należy wrzucać papier?", "niebieskiego", "żółtego"],
+	["Czym są zielone płuca ziemii?", "to lasy", "to największe rzeki"],
 	[
 		"Co jest przyczyną zanieczyszczenia wody?",
 		"wrzucanie do niej odpadów i scieków",
@@ -61,13 +37,25 @@ var origin_transform: Transform
 var correct_side
 var points = 0
 
+
+func _process(_dt):
+	get_node("Player").set_level_specific_text(
+		"punkty: %d, pozostalo %d pytan" % [points, questions.size()]
+	)
+
+func _exit_tree():
+	get_node("Player").set_level_specific_text("")
+
+
 func randi_range(from, to):
 	randomize()
 	return floor(rand_range(from, to + 1))
 
+
 func _ready() -> void:
 	origin_transform = get_node("Player").transform
 	randomize_question()
+
 
 func randomize_question():
 	var left_anwser = get_node("LeftPlate/Answer")
@@ -84,13 +72,14 @@ func randomize_question():
 		left_anwser.text = active_questions[question_index][2]
 		right_answer.text = active_questions[question_index][1]
 
+
 func trigger_anwser(side):
 	if side == correct_side:
 		points = points + 1
 		active_questions.pop_at(question_index)
 	else:
 		points = 0
-		active_questions = questions # TODO: clone
+		active_questions = questions  # TODO: clone
 	randomize_question()
 	get_node("Player").transform = origin_transform
 	print(points)
