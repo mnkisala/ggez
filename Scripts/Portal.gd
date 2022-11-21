90dde0f8cc81f0dcf91fc2c115de7b278014a2ea
Foxyg3n <piotrek.suchecki@vp.pl>
2022-11-21 15:57:14 +0100

    Dodane kody do odblokowywania portali

--- Scripts/Portal.gd
+++ Scripts/Portal.gd
@@ -1,7 +1,7 @@
 extends Spatial
 
 export(LevelEnum.Level) var target = LevelEnum.Level.NULL
-var __active = false
+var activeChange = false
 
 const COLOR_ON = Color(0.3, 1.0, 0.1, 0.3)
 const COLOR_OFF = Color(1.0, 0.1, 0.2, 0.3)
@@ -20,8 +20,25 @@ func _ready():
 		get_node("OmniLight").light_color = COLOR_OFF
 
 
+func setActiveColor(active):
+	if GameManager.isPortalTargetActive(target):
+		get_node("Particles").process_material.color = COLOR_ON
+		get_node("OmniLight").light_color = COLOR_ON
+	else:
+		get_node("Particles").process_material.color = COLOR_OFF
+		get_node("OmniLight").light_color = COLOR_OFF
+
 func _process(dt):
 	# mrygansko
+	if activeChange != GameManager.__portalTarget[target]:
+		activeChange = GameManager.__portalTarget[target]
+		if activeChange:
+			get_node("Particles").process_material.color = COLOR_ON
+			get_node("OmniLight").light_color = COLOR_ON
+		else:
+			get_node("Particles").process_material.color = COLOR_OFF
+			get_node("OmniLight").light_color = COLOR_OFF
+		
 	var light = get_node("OmniLight")
 	light.light_energy = max(
 		0.01, lerp(light.light_energy, BASE_LIGHT + (randf() * 2 - 1.0) * 3.0, dt * 20.0 * randf())
@@ -29,5 +46,5 @@ func _process(dt):
 
 
 func _on_Area_body_entered(body: Node):
-	print("teleportation: ", body, " -> ", target)
-	GameManager.changeScene(target)
+	if GameManager.isPortalTargetActive(target):
+		GameManager.changeScene(target)
