191eb1ddca3399cd2ed72b422ca3930c9801ef99
mnkisala <mnkisala@gmail.com>
2022-11-24 18:43:50 +0100

    fix: questionwall kolizja po usunieciu

--- Scripts/QuestionWall.gd
+++ Scripts/QuestionWall.gd
@@ -4,11 +4,14 @@ class_name QuestionWall
 export(String, MULTILINE) var question setget set_question
 export(bool) var right_correct_anwser = true
 
+
 func _ready():
 	_set_correct_anwser(right_correct_anwser)
-	
+
+
 func set_question(question):
 	get_node("Sign").set_text(question)
+
 
 func _set_correct_anwser(correct):
 	if correct:
@@ -16,9 +19,10 @@ func _set_correct_anwser(correct):
 	else:
 		get_node("QuestionLeftButton").correct = true
 
+
 func disable():
 	visible = false
 	set_process(false)
+	get_parent().remove_child(self)
 	get_node("QuestionLeftButton/CollisionShape").disabled = true
 	get_node("QuestionRightButton/CollisionShape").disabled = true
-	
