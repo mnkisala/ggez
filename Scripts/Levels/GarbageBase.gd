4cc34f774f15cc500861860708f0c3cbb8b6732d
mnkisala <mnkisala@gmail.com>
2022-11-24 16:27:12 +0100

    garbagebase: wyswietlaj punkty

--- Scripts/Levels/GarbageBase.gd
+++ Scripts/Levels/GarbageBase.gd
@@ -1,5 +1,8 @@
 extends Spatial
 
+func _exit_tree():
+	get_node('player').set_level_specific_text("")
 
-func _ready():
-	pass
+
+func _process(_dt):
+	get_node('player').set_level_specific_text("punkty: %d" %  GameManager.player_state.points)
