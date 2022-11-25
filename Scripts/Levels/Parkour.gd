2a2cf07242f7d7a5938805894ae4f2835d3b301e
mnkisala <mnkisala@gmail.com>
2022-11-25 22:00:39 +0100

    parkour: grywalnosc! :)

--- Scripts/Levels/Parkour.gd
+++ Scripts/Levels/Parkour.gd
@@ -4,7 +4,11 @@ var chemicalias_remaining = 0
 
 
 func _process(_dt):
-	get_node('player').set_level_specific_text("pozostalo %s chemikaliow do wylaczenia" %  chemicalias_remaining)
+	if (chemicalias_remaining > 0):
+		get_node('player').set_level_specific_text("pozostalo %s chemikaliow do wylaczenia" %  chemicalias_remaining)
+	else:
+		get_node('player').set_level_specific_text("")
+		get_node("Portal").enable()
 
 func _exit_tree():
 	get_node('player').set_level_specific_text("")
